3748198f19c6bcd4f40897629f4b3c5dc1381a59
出久 <118390636+asim1909@users.noreply.github.com>
2023-01-18 21:24:32 +0530

    Update sWAP-cASE.py

--- sWAP-cASE.py
+++ sWAP-cASE.py
@@ -27,10 +27,7 @@
 def swap_case(s):
     return s.swapcase()
 
-if __name__ == '__main__':
-    s = input()
-    result = swap_case(s)
-    print(result)
+# ----------------------------------------------------------
 
 if __name__ == '__main__':
     s = input()
